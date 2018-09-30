ddc78f57d72462ab0e75fc61f27d18029fcaae3c
Ashutosh Sahoo <15648046+ashutoshsahoo@users.noreply.github.com>
2018-09-30 23:09:09 +0530

    added prompt provider bean

--- src/main/java/in/ashu/robot/RobotAppApplication.java
+++ src/main/java/in/ashu/robot/RobotAppApplication.java
@@ -1,12 +1,18 @@
 package in.ashu.robot;
 
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStyle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.shell.jline.PromptProvider;
 
 /**
  * Main entry point for the application.
  *
- * <p>Creates the application context and start the REPL.</p>
+ * <p>
+ * Creates the application context and start the REPL.
+ * </p>
  *
  * @author ashutosh
  */
@@ -16,5 +22,10 @@ public class RobotAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RobotAppApplication.class, args);
+	}
+
+	@Bean
+	public PromptProvider myPromptProvider() {
+		return () -> new AttributedString("robot:>", AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW));
 	}
 }
